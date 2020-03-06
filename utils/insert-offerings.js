4630ab047d956e5bb0fd3f514dcce0f3aa55a392
ericposas <web_dev@tutanota.com>
2020-03-06 17:58:58 -0600

    updated offerings insert script

--- utils/insert-offerings.js
+++ utils/insert-offerings.js
@@ -11,12 +11,58 @@ let mongoConnectionString = (
 )
 console.log(mongoConnectionString)
 
+const light = [
+  "Consumer Packaged Goods",
+  "Beverage Processing",
+  "Dairy Processing",
+  "Meat Poultry and Seafood Processing",
+  "Brewery & Winery",
+  "Grain & Oilseed Processing",
+  "Biofuels & Ethanol",
+  "Sugar Processing",
+  "Mech/Elec/Eqt Mfg.",
+  "Plastic Products",
+  "General Manufacturing",
+  "Pharmaceutical",
+  "Corrugated Box",
+  "Wood Products",
+  "General Electronics",
+  "Microelectronics",
+  "Rail Transportation",
+  "Aerospc Defense Mfg",
+  "Automotive",
+  "Tire & Rubber Products",
+  "Agricultural & Construction Equipment",
+  "Commercial Buildings",
+  "New Construction",
+  "Education Facilities",
+  "Hospitals & Health",
+  "Hotels and Resorts",
+  "District Energy",
+  "General Gov't",
+  "Long Term Care Facil",
+  "Data Centers",
+  "Casinos",
+]
+const heavy = [
+  "Coal Fired",
+  "Gas Fired",
+  "Geothermal",
+  "Nuclear",
+  "Industrial Gases",
+  "Ammonia & Fertilizer",
+  "Specialty Chemicals",
+  "Aluminum",
+  "Steel Mills",
+  "Investment Casting"
+]
+
 let offerings = [
-  { divisions: ['light'], name: 'Water Safety Intelligence' },
-  { divisions: ['light'], name: 'Water Quality (Light)' },
-  { divisions: ['light'], name: 'Water Quality (Heavy)' },
-  { divisions: ['light', 'heavy'], name: 'Financial Impact' },
-  { divisions: ['light'], name: 'OMNI' }
+  { segments: light, name: 'Water Safety Intelligence' },
+  { segments: light, name: 'Water Quality (Light)' },
+  { segments: heavy, name: 'Water Quality (Heavy)' },
+  { segments: light.concat(heavy), name: 'Financial Impact' },
+  { segments: heavy, name: 'OMNI' }
 ]
 
 const insertEntry = async offering => {
